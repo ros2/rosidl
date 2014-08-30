c11c77e96f471289f529e5d5401e93096f995b1e
Dirk Thomas <dthomas@osrfoundation.org>
2014-08-30 16:11:07 -0700

    use normalize_path()

--- rosidl_generator_cpp/rosidl_generator_cpp-extras.cmake
+++ rosidl_generator_cpp/rosidl_generator_cpp-extras.cmake
@@ -5,4 +5,6 @@ ament_register_extension("rosidl_generate_interfaces" "rosidl_generator_cpp"
   "rosidl_generator_cpp_generate_interfaces.cmake")
 
 set(rosidl_generator_cpp_BIN "${rosidl_generator_cpp_DIR}/../../../lib/rosidl_generator_cpp/rosidl_generator_cpp")
+normalize_path(rosidl_generator_cpp_BIN "${rosidl_generator_cpp_BIN}")
 set(rosidl_generator_cpp_TEMPLATE_DIR "${rosidl_generator_cpp_DIR}/../resource")
+normalize_path(rosidl_generator_cpp_TEMPLATE_DIR "${rosidl_generator_cpp_TEMPLATE_DIR}")

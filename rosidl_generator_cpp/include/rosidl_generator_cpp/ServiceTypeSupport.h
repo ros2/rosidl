6fa4ca5ec489a51d7bca85c0699e1073992cf003
Esteve Fernandez <esteve@osrfoundation.org>
2015-03-04 13:56:39 -0800

    Added get_service_type_support_handle()

--- rosidl_generator_cpp/include/rosidl_generator_cpp/ServiceTypeSupport.h
+++ rosidl_generator_cpp/include/rosidl_generator_cpp/ServiceTypeSupport.h
@@ -1,17 +1,28 @@
+/* Copyright 2015 Open Source Robotics Foundation, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef __rosidl_generator_cpp__ServiceTypeSupport__h__
 #define __rosidl_generator_cpp__ServiceTypeSupport__h__
 
+#include <rosidl_generator_c/service_type_support.h>
+
 namespace rosidl_generator_cpp
 {
 
-typedef struct ServiceTypeSupportHandle {
-  const char * _typesupport_identifier;
-  const void * _data;
-} ServiceTypeSupportHandle;
-
 template<typename T>
-const ServiceTypeSupportHandle& get_service_type_support_handle();
+const rosidl_service_type_support_t * get_service_type_support_handle();
 
 }  // namespace rosidl_generator_cpp
 

71344967498bd2ecb8acb09953a3509d1d30c737
William Woodall <william@osrfoundation.org>
2015-03-04 16:33:33 -0800

    replacing ros_middleware with rmw

--- include/rosidl_typesupport_introspection_cpp/ServiceIntrospection.h
+++ include/rosidl_typesupport_introspection_cpp/ServiceIntrospection.h
@@ -2,13 +2,16 @@
 #ifndef __rosidl_typesupport_introspection_cpp__ServiceIntrospection__h__
 #define __rosidl_typesupport_introspection_cpp__ServiceIntrospection__h__
 
+#include <rosidl_generator_c/service_type_support.h>
+#include <rosidl_generator_cpp/ServiceTypeSupport.h>
+
 namespace rosidl_typesupport_introspection_cpp
 {
 
 extern const char * typesupport_introspection_identifier;
 
 template<typename T>
-const rosidl_generator_cpp::ServiceTypeSupportHandle& get_service_type_support_handle();
+const rosidl_service_type_support_t * get_service_type_support_handle();
 
 }  // namespace rosidl_typesupport_introspection_cpp
 

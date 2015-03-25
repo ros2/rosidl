8acb69e416c9763d5047c1ad9c133c13536c6fcc
William Woodall <william@osrfoundation.org>
2015-03-25 04:14:29 -0700

    change how the impl specific header is included
    previously it was included along side this header
    by other headers, but now it is tail included to
    simplify the pattern. The tail included header,
    rosidl_generator_cpp/
    get_type_support_handle_impl.hpp
    is actually provided by the DDS vendor specific
    implementations, not by rosidl_generator_cpp, but
    this namespace is used because they each provide
    a version of the same header, and only one can
    be included.

--- rosidl_generator_cpp/include/rosidl_generator_cpp/MessageTypeSupport.h
+++ rosidl_generator_cpp/include/rosidl_generator_cpp/MessageTypeSupport.h
@@ -17,13 +17,22 @@
 #define __rosidl_generator_cpp__MessageTypeSupport__h__
 
 #include <rosidl_generator_c/message_type_support.h>
+#include <rosidl_generator_c/visibility_control.h>
 
 namespace rosidl_generator_cpp
 {
 
 template<typename T>
+ROSIDL_PUBLIC
 const rosidl_message_type_support_t * get_type_support_handle();
 
 }  // namespace rosidl_generator_cpp
+
+// Tail include the implementation for this function.
+// This include is provided by the implementation and therefore only
+// one version of this header will get included from the path.
+// This header maps the get_type_support_handle function to an implementation
+// specific version of that function.
+#include <rosidl_generator_cpp/get_type_support_handle_impl.hpp>
 
 #endif  // __rosidl_generator_cpp__MessageTypeSupport__h__

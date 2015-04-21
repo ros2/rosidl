3b3fe7fcbeca2460bc1078dc63d5bc183cb83b8e
Dirk Thomas <dthomas@osrfoundation.org>
2015-04-21 15:39:38 -0700

    fix visibility of service typesupport on Windows

--- rosidl_generator_c/include/rosidl_generator_c/service_type_support.h
+++ rosidl_generator_c/include/rosidl_generator_c/service_type_support.h
@@ -15,12 +15,14 @@
 #ifndef ROSIDL_GENERATOR_C_ROSIDL_GENERATOR_C_SERVICE_TYPE_SUPPORT_H_
 #define ROSIDL_GENERATOR_C_ROSIDL_GENERATOR_C_SERVICE_TYPE_SUPPORT_H_
 
+#include "rosidl_generator_c/visibility_control.h"
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-typedef struct rosidl_service_type_support_t
+typedef struct ROSIDL_PUBLIC rosidl_service_type_support_t
 {
   const char * typesupport_identifier;
   const void * data;

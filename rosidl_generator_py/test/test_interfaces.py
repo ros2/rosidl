867191e80fe6f9f952d9da70912f5cbcee098b7e
Esteve Fernandez <esteve@osrfoundation.org>
2016-01-19 17:39:29 -0800

    Use single quotes

--- rosidl_generator_py/test/test_interfaces.py
+++ rosidl_generator_py/test/test_interfaces.py
@@ -23,42 +23,42 @@ def test_strings():
     a = Strings()
 
     assert(a.empty_string is None)
-    assert(a.def_string == "Hello world!")
+    assert(a.def_string == 'Hello world!')
 
 
 def test_invalid_attribute():
     a = Strings()
 
-    assert_raises(AttributeError, setattr, a, "invalid_string1", "foo")
+    assert_raises(AttributeError, setattr, a, 'invalid_string1', 'foo')
 
-    assert_raises(AttributeError, getattr, a, "invalid_string2")
+    assert_raises(AttributeError, getattr, a, 'invalid_string2')
 
 
 def test_constructor():
-    a = Strings(empty_string="foo")
+    a = Strings(empty_string='foo')
 
-    assert(a.empty_string == "foo")
+    assert(a.empty_string == 'foo')
 
-    assert_raises(AssertionError, Strings, unknown_field="test")
+    assert_raises(AssertionError, Strings, unknown_field='test')
 
 
 def test_constants():
     assert(Constants.X == 123)
     assert(Constants.Y == -123)
-    assert(Constants.FOO == "foo")
+    assert(Constants.FOO == 'foo')
 
-    assert_raises(AttributeError, setattr, Constants, "FOO", "bar")
+    assert_raises(AttributeError, setattr, Constants, 'FOO', 'bar')
 
 
 def test_default_values():
     a = Strings()
 
     assert(a.empty_string is None)
-    assert(a.def_string == "Hello world!")
-    a.def_string = "Bye world"
-    assert(a.def_string == "Bye world")
-    assert(Strings.DEF_STRING__DEFAULT == "Hello world!")
-    assert(a.DEF_STRING__DEFAULT == "Hello world!")
-    assert_raises(AttributeError, setattr, Strings, "DEF_STRING__DEFAULT", "bar")
+    assert(a.def_string == 'Hello world!')
+    a.def_string = 'Bye world'
+    assert(a.def_string == 'Bye world')
+    assert(Strings.DEF_STRING__DEFAULT == 'Hello world!')
+    assert(a.DEF_STRING__DEFAULT == 'Hello world!')
+    assert_raises(AttributeError, setattr, Strings, 'DEF_STRING__DEFAULT', 'bar')
 
     assert(Various.TWO_UINT16_VALUE__DEFAULT == (5, 23))

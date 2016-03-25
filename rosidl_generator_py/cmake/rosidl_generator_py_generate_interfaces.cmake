4b04aae070b7850442976568646390d7fdd782b2
Dirk Thomas <dthomas@osrfoundation.org>
2016-03-25 00:40:27 -0700

    add missing target dependency

--- rosidl_generator_py/cmake/rosidl_generator_py_generate_interfaces.cmake
+++ rosidl_generator_py/cmake/rosidl_generator_py_generate_interfaces.cmake
@@ -168,6 +168,18 @@ add_custom_command(
   VERBATIM
 )
 
+if(TARGET ${rosidl_generate_interfaces_TARGET}${_target_suffix})
+  message(WARNING "Custom target ${rosidl_generate_interfaces_TARGET}${_target_suffix} already exists")
+else()
+  add_custom_target(
+    ${rosidl_generate_interfaces_TARGET}${_target_suffix}
+    DEPENDS
+    ${_generated_msg_py_files}
+    ${_generated_msg_c_files}
+    ${_generated_srv_files}
+  )
+endif()
+
 macro(set_properties _build_type)
   set_target_properties(${_msg_name}${_pyext_suffix} PROPERTIES
     COMPILE_FLAGS "${_extension_compile_flags}"
@@ -210,6 +222,7 @@ foreach(_generated_msg_c_ts_file ${_generated_msg_c_ts_files})
 
     add_dependencies(
       ${_msg_name}${_pyext_suffix}
+      ${rosidl_generate_interfaces_TARGET}${_target_suffix}
       ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
     )
 
@@ -266,18 +279,3 @@ foreach(_generated_msg_c_ts_file ${_generated_msg_c_ts_files})
       DESTINATION "${PYTHON_INSTALL_DIR}/${PROJECT_NAME}/${_msg_package_dir2}")
   endif()
 endforeach()
-
-if(TARGET ${rosidl_generate_interfaces_TARGET}${_target_suffix})
-  message(WARNING "Custom target ${rosidl_generate_interfaces_TARGET}${_target_suffix} already exists")
-else()
-  add_custom_target(
-    ${rosidl_generate_interfaces_TARGET}${_target_suffix}
-    DEPENDS
-    ${_generated_msg_py_files}
-    ${_generated_msg_c_files}
-    ${_generated_srv_files}
-    ${_generated_extension_files}
-    ${_extension_dependencies}
-    ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
-  )
-endif()

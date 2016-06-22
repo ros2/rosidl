ed952eda59662192369d1ad463666d0bdf11abbc
Esteve Fernandez <esteve@apache.org>
2016-06-22 03:02:31 +0200

    Enable FastRTPS for C (#128)

--- rosidl_generator_py/cmake/rosidl_generator_py_get_typesupports.cmake
+++ rosidl_generator_py/cmake/rosidl_generator_py_get_typesupports.cmake
@@ -14,10 +14,8 @@
 
 macro(accumulate_typesupports)
   set(_typesupport_impl "")
-  if(NOT rmw_implementation STREQUAL "rmw_fastrtps_cpp")
-    get_rmw_typesupport(_typesupport_impl ${rmw_implementation} LANGUAGE "C")
-    list(APPEND _typesupport_impls ${_typesupport_impl})
-  endif()
+  get_rmw_typesupport(_typesupport_impl ${rmw_implementation} LANGUAGE "C")
+  list(APPEND _typesupport_impls ${_typesupport_impl})
 endmacro()
 
 macro(rosidl_generator_py_get_typesupports TYPESUPPORT_IMPLS)

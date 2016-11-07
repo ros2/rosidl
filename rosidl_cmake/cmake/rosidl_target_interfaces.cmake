f40787409565678f244493a2cdc58855d754ef5a
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-11-07 14:33:05 -0800

    line length

--- rosidl_cmake/cmake/rosidl_target_interfaces.cmake
+++ rosidl_cmake/cmake/rosidl_target_interfaces.cmake
@@ -42,7 +42,8 @@ function(rosidl_target_interfaces target interface_target typesupport_name)
   set(typesupport_target "${interface_target}__${typesupport_name}")
   if(NOT TARGET ${typesupport_target})
     message(FATAL_ERROR "rosidl_target_interfaces() the second argument concattenated "
-      "with the third argument using double underscores must be a valid target name")
+      "with the third argument using double underscores must be "
+      "a valid target name")
   endif()
 
   add_dependencies(${target} ${interface_target})

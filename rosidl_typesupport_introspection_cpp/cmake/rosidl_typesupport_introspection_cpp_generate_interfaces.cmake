93b6ba9d83a6e9d8130d3e3a9a8c55888b079a97
Morgan Quigley <morgan@osrfoundation.org>
2016-12-08 10:31:59 -0800

    c++14

--- rosidl_typesupport_introspection_cpp/cmake/rosidl_typesupport_introspection_cpp_generate_interfaces.cmake
+++ rosidl_typesupport_introspection_cpp/cmake/rosidl_typesupport_introspection_cpp_generate_interfaces.cmake
@@ -87,7 +87,7 @@ if(WIN32)
 endif()
 if(NOT WIN32)
   set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix}
-    PROPERTIES COMPILE_FLAGS "-std=c++11 -Wall -Wextra")
+    PROPERTIES COMPILE_FLAGS "-std=c++14 -Wall -Wextra")
 endif()
 target_include_directories(${rosidl_generate_interfaces_TARGET}${_target_suffix}
   PUBLIC

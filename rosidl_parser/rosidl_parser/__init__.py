6a06aaeedc80606c9672e6ae97195a24c4cfe21b
Mikael Arguedas <mikael.arguedas@gmail.com>
2017-02-07 11:57:27 -0800

    use simple regex

--- rosidl_parser/rosidl_parser/__init__.py
+++ rosidl_parser/rosidl_parser/__init__.py
@@ -566,26 +566,17 @@ def parse_primitive_value_string(type_, value_string):
         for quote in ['"', "'"]:
             if value_string.startswith(quote) and value_string.endswith(quote):
                 value_string = value_string[1:-1]
-                tmp_value_string = value_string
-                clear_escaping = False
-                while True:
-                    index = tmp_value_string.find(quote)
-                    if index < 0:
-                        # no inner quote, proceed
-                        break
-                    elif tmp_value_string[index - 1] != '\\' or index == 0:
-                        # inner quote not escaped, raise an InvalidValue Exception
-                        raise InvalidValue(
-                            primitive_type,
-                            value_string,
-                            'string is not properly escaped, the character `' +
-                            tmp_value_string[index] + '` should be escaped')
-                    else:
-                        clear_escaping = True
-                    tmp_value_string = tmp_value_string[index + 1:]
-                if clear_escaping:
-                    # clears escaping, each language will handle it differently
+                if quote == '"':
+                    match = re.search(r'(?<!\\)"', value_string)
+                else:
+                    match = re.search(r"(?<!\\)'", value_string)
+                if match is None:
                     value_string = value_string.replace('\\' + quote, quote)
+                else:
+                    raise InvalidValue(
+                        primitive_type,
+                        value_string,
+                        'string inner quotes not properly escaped')
                 break
 
         # check that value is in valid range

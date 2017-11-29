5fae63ecfb21fdeb55f141074c0582145c7367a1
Mikael Arguedas <mikael@osrfoundation.org>
2017-11-29 15:31:22 -0800

    test all primitive types

--- rosidl_generator_cpp/test/test_msg_initialization.cpp
+++ rosidl_generator_cpp/test/test_msg_initialization.cpp
@@ -18,6 +18,8 @@
 
 #include <string>
 
+#include "rosidl_generator_cpp/msg/primitives_default.hpp"
+#include "rosidl_generator_cpp/msg/primitives_static.hpp"
 #include "rosidl_generator_cpp/msg/various.hpp"
 
 template<typename Callable>
@@ -44,43 +46,93 @@ make_scope_exit(Callable callable)
   auto STRING_JOIN(scope_exit_, __LINE__) = make_scope_exit([&]() {code;})
 
 TEST(Test_msg_initialization, no_arg_constructor) {
-  rosidl_generator_cpp::msg::Various def;
+  rosidl_generator_cpp::msg::PrimitivesDefault primitives_def;
+  ASSERT_TRUE(primitives_def.bool_value);
+  ASSERT_EQ(50, primitives_def.byte_value);
+  ASSERT_EQ(100, primitives_def.char_value);
+  ASSERT_EQ(1.125f, primitives_def.float32_value);
+  ASSERT_EQ(1.125, primitives_def.float64_value);
+  ASSERT_EQ(-50, primitives_def.int8_value);
+  ASSERT_EQ(200, primitives_def.uint8_value);
+  ASSERT_EQ(-1000, primitives_def.int16_value);
+  ASSERT_EQ(2000, primitives_def.uint16_value);
+  ASSERT_EQ(-30000L, primitives_def.int32_value);
+  ASSERT_EQ(60000UL, primitives_def.uint32_value);
+  ASSERT_EQ(-40000000LL, primitives_def.int64_value);
+  ASSERT_EQ(50000000ULL, primitives_def.uint64_value);
+  ASSERT_EQ("bar", primitives_def.string_value);
 
-  ASSERT_EQ(1.125f, def.float32_value);
-  ASSERT_EQ(2.4, def.float64_value);
-  ASSERT_EQ(0ULL, def.uint64_value);
-  ASSERT_EQ("bar", def.string_value);
-  ASSERT_TRUE(std::all_of(def.float32_arr.begin(), def.float32_arr.end(), [](float i) {
-      return 0.0f == i;
-    }));
-  ASSERT_EQ(8.5, def.float64_arr[0]);
-  ASSERT_EQ(1.2, def.float64_arr[1]);
-  ASSERT_EQ(3.4, def.float64_arr[2]);
-  ASSERT_TRUE(std::all_of(def.string_arr.begin(), def.string_arr.end(), [](std::string i) {
-      return "" == i;
-    }));
-  ASSERT_EQ(2UL, def.unbounded.size());
-  ASSERT_EQ(1.0f, def.unbounded[0]);
-  ASSERT_EQ(2.0f, def.unbounded[1]);
-  ASSERT_EQ(0UL, def.bounded_no_def.size());
-  ASSERT_EQ(2UL, def.bounded_def.size());
-  ASSERT_EQ(3.0f, def.bounded_def[0]);
-  ASSERT_EQ(4.0f, def.bounded_def[1]);
-  ASSERT_EQ(0UL, def.vec3.x);
-  ASSERT_EQ(45UL, def.vec3.y);
-  ASSERT_EQ(0UL, def.vec3.z);
-  ASSERT_EQ(2UL, def.vec3_fixed.size());
-  ASSERT_EQ(0UL, def.vec3_fixed[0].x);
-  ASSERT_EQ(0UL, def.vec3_fixed[1].x);
-  ASSERT_EQ(45UL, def.vec3_fixed[0].y);
-  ASSERT_EQ(45UL, def.vec3_fixed[1].y);
-  ASSERT_EQ(0UL, def.vec3_fixed[0].z);
-  ASSERT_EQ(0UL, def.vec3_fixed[1].z);
-  ASSERT_EQ(0UL, def.vec3_unbounded.size());
-  ASSERT_EQ(0UL, def.vec3_bounded.size());
+  rosidl_generator_cpp::msg::PrimitivesStatic primitives_static;
+  ASSERT_FALSE(primitives_static.bool_value);
+  ASSERT_EQ(0, primitives_static.byte_value);
+  ASSERT_EQ(0, primitives_static.char_value);
+  ASSERT_EQ(0.0f, primitives_static.float32_value);
+  ASSERT_EQ(0.0, primitives_static.float64_value);
+  ASSERT_EQ(0, primitives_static.int8_value);
+  ASSERT_EQ(0, primitives_static.uint8_value);
+  ASSERT_EQ(0, primitives_static.int16_value);
+  ASSERT_EQ(0, primitives_static.uint16_value);
+  ASSERT_EQ(0L, primitives_static.int32_value);
+  ASSERT_EQ(0UL, primitives_static.uint32_value);
+  ASSERT_EQ(0LL, primitives_static.int64_value);
+  ASSERT_EQ(0ULL, primitives_static.uint64_value);
+
+  rosidl_generator_cpp::msg::Various various_def;
+
+  ASSERT_EQ(1.125f, various_def.float32_value);
+  ASSERT_EQ(2.4, various_def.float64_value);
+  ASSERT_EQ(0ULL, various_def.uint64_value);
+  ASSERT_EQ("bar", various_def.string_value);
+  ASSERT_TRUE(std::all_of(
+      various_def.float32_arr.begin(), various_def.float32_arr.end(), [](float i) {
+        return 0.0f == i;
+      }));
+  ASSERT_EQ(8.5, various_def.float64_arr[0]);
+  ASSERT_EQ(1.2, various_def.float64_arr[1]);
+  ASSERT_EQ(3.4, various_def.float64_arr[2]);
+  ASSERT_TRUE(std::all_of(
+      various_def.string_arr.begin(), various_def.string_arr.end(), [](std::string i) {
+        return "" == i;
+      }));
+  ASSERT_EQ(2UL, various_def.unbounded.size());
+  ASSERT_EQ(1.0f, various_def.unbounded[0]);
+  ASSERT_EQ(2.0f, various_def.unbounded[1]);
+  ASSERT_EQ(0UL, various_def.bounded_no_def.size());
+  ASSERT_EQ(2UL, various_def.bounded_def.size());
+  ASSERT_EQ(3.0f, various_def.bounded_def[0]);
+  ASSERT_EQ(4.0f, various_def.bounded_def[1]);
+  ASSERT_EQ(0UL, various_def.vec3.x);
+  ASSERT_EQ(45UL, various_def.vec3.y);
+  ASSERT_EQ(0UL, various_def.vec3.z);
+  ASSERT_EQ(2UL, various_def.vec3_fixed.size());
+  ASSERT_EQ(0UL, various_def.vec3_fixed[0].x);
+  ASSERT_EQ(0UL, various_def.vec3_fixed[1].x);
+  ASSERT_EQ(45UL, various_def.vec3_fixed[0].y);
+  ASSERT_EQ(45UL, various_def.vec3_fixed[1].y);
+  ASSERT_EQ(0UL, various_def.vec3_fixed[0].z);
+  ASSERT_EQ(0UL, various_def.vec3_fixed[1].z);
+  ASSERT_EQ(0UL, various_def.vec3_unbounded.size());
+  ASSERT_EQ(0UL, various_def.vec3_bounded.size());
 }
 
 TEST(Test_msg_initialization, all_constructor) {
+  rosidl_generator_cpp::msg::PrimitivesDefault primitives_def(
+    rosidl_generator_cpp::MessageInitialization::ALL);
+  ASSERT_TRUE(primitives_def.bool_value);
+  ASSERT_EQ(50, primitives_def.byte_value);
+  ASSERT_EQ(100, primitives_def.char_value);
+  ASSERT_EQ(1.125f, primitives_def.float32_value);
+  ASSERT_EQ(1.125, primitives_def.float64_value);
+  ASSERT_EQ(-50, primitives_def.int8_value);
+  ASSERT_EQ(200, primitives_def.uint8_value);
+  ASSERT_EQ(-1000, primitives_def.int16_value);
+  ASSERT_EQ(2000, primitives_def.uint16_value);
+  ASSERT_EQ(-30000L, primitives_def.int32_value);
+  ASSERT_EQ(60000UL, primitives_def.uint32_value);
+  ASSERT_EQ(-40000000LL, primitives_def.int64_value);
+  ASSERT_EQ(50000000ULL, primitives_def.uint64_value);
+  ASSERT_EQ("bar", primitives_def.string_value);
+
   rosidl_generator_cpp::msg::Various def(
     rosidl_generator_cpp::MessageInitialization::ALL);
 
@@ -119,6 +171,23 @@ TEST(Test_msg_initialization, all_constructor) {
 }
 
 TEST(Test_msg_initialization, zero_constructor) {
+  rosidl_generator_cpp::msg::PrimitivesDefault primitives_def(
+    rosidl_generator_cpp::MessageInitialization::ZERO);
+  ASSERT_FALSE(primitives_def.bool_value);
+  ASSERT_EQ(0, primitives_def.byte_value);
+  ASSERT_EQ(0, primitives_def.char_value);
+  ASSERT_EQ(0.0f, primitives_def.float32_value);
+  ASSERT_EQ(0.0, primitives_def.float64_value);
+  ASSERT_EQ(0, primitives_def.int8_value);
+  ASSERT_EQ(0, primitives_def.uint8_value);
+  ASSERT_EQ(0, primitives_def.int16_value);
+  ASSERT_EQ(0, primitives_def.uint16_value);
+  ASSERT_EQ(0L, primitives_def.int32_value);
+  ASSERT_EQ(0UL, primitives_def.uint32_value);
+  ASSERT_EQ(0LL, primitives_def.int64_value);
+  ASSERT_EQ(0ULL, primitives_def.uint64_value);
+  ASSERT_EQ("", primitives_def.string_value);
+
   rosidl_generator_cpp::msg::Various def(
     rosidl_generator_cpp::MessageInitialization::ZERO);
 
@@ -157,6 +226,23 @@ TEST(Test_msg_initialization, zero_constructor) {
 }
 
 TEST(Test_msg_initialization, defaults_only_constructor) {
+  rosidl_generator_cpp::msg::PrimitivesDefault primitives_def(
+    rosidl_generator_cpp::MessageInitialization::DEFAULTS_ONLY);
+  ASSERT_TRUE(primitives_def.bool_value);
+  ASSERT_EQ(50, primitives_def.byte_value);
+  ASSERT_EQ(100, primitives_def.char_value);
+  ASSERT_EQ(1.125f, primitives_def.float32_value);
+  ASSERT_EQ(1.125, primitives_def.float64_value);
+  ASSERT_EQ(-50, primitives_def.int8_value);
+  ASSERT_EQ(200, primitives_def.uint8_value);
+  ASSERT_EQ(-1000, primitives_def.int16_value);
+  ASSERT_EQ(2000, primitives_def.uint16_value);
+  ASSERT_EQ(-30000L, primitives_def.int32_value);
+  ASSERT_EQ(60000UL, primitives_def.uint32_value);
+  ASSERT_EQ(-40000000LL, primitives_def.int64_value);
+  ASSERT_EQ(50000000ULL, primitives_def.uint64_value);
+  ASSERT_EQ("bar", primitives_def.string_value);
+
   char * memory = new char[sizeof(rosidl_generator_cpp::msg::Various)];
   ASSERT_NE(memory, nullptr);
   std::memset(memory, 0xfe, sizeof(rosidl_generator_cpp::msg::Various));

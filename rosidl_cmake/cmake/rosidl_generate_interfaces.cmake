2ffdeed8e9b31892508f1cb5d18ca58a6f88b0f0
Mikael Arguedas <mikael@osrfoundation.org>
2017-12-01 11:40:03 -0800

    enforce membership to rosidl_interface_packages

--- rosidl_cmake/cmake/rosidl_generate_interfaces.cmake
+++ rosidl_cmake/cmake/rosidl_generate_interfaces.cmake
@@ -114,6 +114,16 @@ macro(rosidl_generate_interfaces target)
   )
 
   if(NOT _ARG_SKIP_INSTALL)
+    set(_interface_packages_group_name "rosidl_interface_packages")
+    if(NOT _AMENT_PACKAGE_NAME)
+      ament_package_xml()
+    endif()
+    if(NOT _interface_packages_group_name IN_LIST ${_AMENT_PACKAGE_NAME}_MEMBER_OF_GROUPS)
+      message(FATAL_ERROR
+        "Packages installing interfaces must include \
+        '<member_of_group>${_interface_packages_group_name}</member_of_group>' in their package.xml"
+      )
+    endif()
     # register interfaces with the ament index
     set(_idl_files_lines)
     foreach(_idl_file ${_ARG_UNPARSED_ARGUMENTS})

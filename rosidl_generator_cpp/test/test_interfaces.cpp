47058465ba35a7a84a35ae60d211633300adcf03
Mikael Arguedas <mikael@osrfoundation.org>
2017-12-10 04:29:09 +0000

    [rosidl_generator_cpp] test string array default values (wip looks like escaped IDL strings are broken in C++)

--- rosidl_generator_cpp/test/test_interfaces.cpp
+++ rosidl_generator_cpp/test/test_interfaces.cpp
@@ -556,6 +556,7 @@ TEST(Test_messages, primitives_default) {
 
 // String arrays with default values
 TEST(Test_messages, string_arrays_default) {
+  // rosidl_generator_cpp::msg::StringArrays message;
   rosidl_generator_cpp::msg::StringArrays message;
 
   ASSERT_STREQ("Hello", message.def_string_static_array_value[0].c_str());
@@ -573,6 +574,14 @@ TEST(Test_messages, string_arrays_default) {
   ASSERT_STREQ("wonderful", message.def_string_dynamic_array_value[2].c_str());
   ASSERT_STREQ("world", message.def_string_dynamic_array_value[3].c_str());
   ASSERT_STREQ("!", message.def_string_dynamic_array_value[4].c_str());
+
+  ASSERT_EQ(6ul, message.def_various_commas.size());
+  ASSERT_STREQ("Hel,lo", message.def_various_commas[0].c_str());
+  ASSERT_STREQ(",World", message.def_various_commas[1].c_str());
+  ASSERT_STREQ("abcd", message.def_various_commas[2].c_str());
+  ASSERT_STREQ("", message.def_various_commas[3].c_str());
+  ASSERT_STREQ("", message.def_various_commas[4].c_str());
+  ASSERT_STREQ("!,", message.def_various_commas[5].c_str());
 }
 
 // TODO(mikaelarguedas) reenable this test when bounded strings enforce length

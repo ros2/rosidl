d4e108b8c4babc5a7bf7d8eb8d680f1b69266609
Shane Loretz <sloretz@osrfoundation.org>
2018-09-25 12:37:54 -0700

    Remove to_character_literal since type changed to uint8

--- rosidl_adapter/rosidl_adapter/msg/__init__.py
+++ rosidl_adapter/rosidl_adapter/msg/__init__.py
@@ -81,26 +81,7 @@ def contains_unicode(string):
 def to_literal(idl_type, value):
     if 'string' == idl_type:
         return to_string_literal(value)
-    elif 'char' == idl_type:
-        return to_character_literal(value)
     return value
-
-
-def to_character_literal(value):
-    """Convert string to character literal as described in IDL 4.2 section  7.2.6.2.1 .
-    Value is a value from a .msg char.
-    The return value text defining a character literal to put into idl."""
-    if isinstance(value, int):
-        # msg char is [-128, 127]
-        # Make negative numbers instead be in the range [128, 255] and convert to char.
-        if value < 0:
-            value *= -1
-            value += 127
-        value = chr(value)
-    estr = escape_string(value)
-    if contains_unicode(value):
-        return "L'{0}'".format(estr)
-    return "'{0}'".format(estr)
 
 
 def to_string_literal(string):

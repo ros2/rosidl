d7fe618333bcb56777faedca5846721e640f75af
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-04-16 08:17:00 -0700

    Remove obsolete API (#359)
    * remove obsolete API
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * remove usage of UnknownMessageType
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- rosidl_cmake/rosidl_cmake/__init__.py
+++ rosidl_cmake/rosidl_cmake/__init__.py
@@ -20,9 +20,6 @@ import re
 import sys
 
 import em
-
-from rosidl_parser import BaseType
-from rosidl_parser import PACKAGE_NAME_MESSAGE_TYPE_SEPARATOR
 from rosidl_parser.definition import IdlLocator
 from rosidl_parser.parser import parse_idl_file
 
@@ -35,34 +32,6 @@ def convert_camel_case_to_lower_case_underscore(value):
     # which is preseded by a lower case letter or number
     value = re.sub('([a-z0-9])([A-Z])', r'\1_\2', value)
     return value.lower()
-
-
-def extract_message_types(pkg_name, ros_interface_files, deps):
-    types = []
-
-    for ros_interface_file in ros_interface_files:
-        base_type = _get_base_type(pkg_name, ros_interface_file)
-        if base_type:
-            types.append(base_type)
-
-    for dep in deps:
-        # only take the first : for separation, as Windows follows with a C:\
-        dep_parts = dep.split(':', 1)
-        assert len(dep_parts) == 2, "The dependency '%s' must contain a double colon" % dep
-        pkg_name = dep_parts[0]
-        base_type = _get_base_type(pkg_name, dep_parts[1])
-        if base_type:
-            types.append(base_type)
-
-    return types
-
-
-def _get_base_type(pkg_name, idl_path):
-    idl_filename = os.path.basename(idl_path)
-    msg_name, extension = os.path.splitext(idl_filename)
-    if extension != '.msg':
-        return None
-    return BaseType(pkg_name + PACKAGE_NAME_MESSAGE_TYPE_SEPARATOR + msg_name)
 
 
 def read_generator_arguments(input_file):

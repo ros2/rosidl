ros2/rosidl

commit 416ebd84a678178f8e8941a1961af8abe7514251

change generators to IDL-based pipeline, change char type, separate action types (#334)
* add Python function for IDL-based generators

* update C and C++ message generators to use IDL-based extension point

* update includes in typesupport packages

* replace separate action generation with IDL-based approach

* don't pass .action files to legacy message generators

* temporary use custom map in not yet updated generator

* strip action specific suffixes from include filename

* add action templates to list of dependencies

* match constants with message

* traits

* native path

* escape JSON correctly

* rsplit

* fix warning

* comment fix

* unique include directives

* make pydot optional

* fix linter

* linter

* fix comment

* remove service check for feedback suffix

* use UINT8_MAX

* fix comment

* extend action struct with FeedbackMessage

* update C and C++ introspection typesupport generators to use IDL-based extension point

* add missing symbols

* address linter warnings

* instantiate parser on demand

* Add option to generate_files() for keeping idl filename case (#336)

* remove unused var

* remove comment

* fix missing import introduced during rebase

* change exported variable from <pkg>_INTERFACE_FILES to <pkg>_IDL_FILES and only include .idl files

* keep <pkg>_INTERFACE_FILES for legacy generators

* fix upper string length for nested strings

* fix default value for strings with upper bounds

* fix classification of unbounded sequence of bounded strings

* fix previous fix

* Adds includes for action wrapper IDL messages fields. (#337)

* Adds includes for action wrapper IDL messages fields.

* move includes

* add includes before action

* Deduplicates implicit action includes.

* Use set comprehension instead (nit fix)

* fix adding actions

* Add missing closing brace for 'extern C'

* fix missing service request/response symbols

rosidl_typesupport_introspection_cpp/resource/msg__type_support.cpp.em
@@ -1,139 +1,169 @@
-// generated from rosidl_typesupport_introspection_cpp/resource/msg__type_support.cpp.em
-// generated code does not contain a copyright notice
+@# Included from rosidl_typesupport_introspection_cpp/resource/idl__type_support.cpp.em
+@{
+from rosidl_parser.definition import Array
+from rosidl_parser.definition import BaseString
+from rosidl_parser.definition import BasicType
+from rosidl_parser.definition import BoundedSequence
+from rosidl_parser.definition import NamespacedType
+from rosidl_parser.definition import NestedType
+from rosidl_parser.definition import Sequence
+from rosidl_parser.definition import String
+from rosidl_parser.definition import WString
+from rosidl_cmake import convert_camel_case_to_lower_case_underscore
 
-@#######################################################################
-@# EmPy template for generating <msg>__type_support.cpp files
-@#
-@# Context:
-@#  - spec (rosidl_parser.MessageSpecification)
-@#    Parsed specification of the .msg file
-@#  - subfolder (string)
-@#    The subfolder / subnamespace of the message
-@#    Either 'msg', 'srv' or 'action'
-@#  - get_header_filename_from_msg_name (function)
-@#######################################################################
-@
-#include <array>
-// providing offsetof()
-#include <cstddef>
-#include <string>
-#include <vector>
+include_parts = [package_name] + list(interface_path.parents[0].parts) + \
+    [convert_camel_case_to_lower_case_underscore(interface_path.stem)]
+include_base = '/'.join(include_parts)
 
-#include "rosidl_generator_c/message_type_support_struct.h"
-#include "rosidl_typesupport_cpp/message_type_support.hpp"
-#include "rosidl_typesupport_interface/macros.h"
+header_files = [
+    'array',
+    'cstddef',  # providing offsetof()
+    'string',
+    'vector',
+    'rosidl_generator_c/message_type_support_struct.h',
+    'rosidl_typesupport_cpp/message_type_support.hpp',
+    'rosidl_typesupport_interface/macros.h',
+    include_base + '__struct.hpp',
+    'rosidl_typesupport_introspection_cpp/field_types.hpp',
+    'rosidl_typesupport_introspection_cpp/identifier.hpp',
+    'rosidl_typesupport_introspection_cpp/message_introspection.hpp',
+    'rosidl_typesupport_introspection_cpp/message_type_support_decl.hpp',
+    'rosidl_typesupport_introspection_cpp/visibility_control.h',
+]
+}@
+@[for header_file in header_files]@
+@[    if header_file in include_directives]@
+// already included above
+// @
+@[    else]@
+@{include_directives.add(header_file)}@
+@[    end if]@
+#include "@(header_file)"
+@[end for]@
+@[for ns in message.structure.type.namespaces]@
 
-#include "@(spec.base_type.pkg_name)/@(subfolder)/@(get_header_filename_from_msg_name(spec.base_type.type))__struct.hpp"
-#include "rosidl_typesupport_introspection_cpp/field_types.hpp"
-#include "rosidl_typesupport_introspection_cpp/identifier.hpp"
-#include "rosidl_typesupport_introspection_cpp/message_introspection.hpp"
-#include "rosidl_typesupport_introspection_cpp/message_type_support_decl.hpp"
-#include "rosidl_typesupport_introspection_cpp/visibility_control.h"
-
-namespace @(spec.base_type.pkg_name)
+namespace @(ns)
 {
-
-namespace @(subfolder)
-{
+@[end for]@
 
 namespace rosidl_typesupport_introspection_cpp
 {
 
-@[if spec.fields]@
-@[  for field in spec.fields]@
+@[for member in message.structure.members]@
 @{
-def is_vector_bool(field):
-  return field.type.type == 'bool' and not (field.type.array_size and not field.type.is_upper_bound)
+def is_vector_bool(member):
+    from rosidl_parser.definition import BasicType
+    from rosidl_parser.definition import Sequence
+    return isinstance(member.type, Sequence) and isinstance(member.type.basetype, BasicType) and member.type.basetype.type == 'boolean'
 }@
 @# exclude std::vector<bool> because of specialization in their API
-@[    if field.type.is_array and not is_vector_bool(field)]@
+@[  if isinstance(member.type, NestedType) and not is_vector_bool(member)]@
 @{
-# from rosidl_generator_cpp import msg_type_only_to_cpp
-# type = msg_type_only_to_cpp(field.type)
-default_type = str(field.type.pkg_name) + '::msg::' + field.type.type
-type_ = cpp_primitives.get(field.type.type, default_type)
-if field.type.type == 'string':
+from rosidl_generator_cpp import  MSG_TYPE_TO_CPP
+if isinstance(member.type.basetype, BasicType):
+    type_ = MSG_TYPE_TO_CPP[member.type.basetype.type]
+elif isinstance(member.type.basetype, String):
     type_ = 'std::string'
+elif isinstance(member.type.basetype, WString):
+    assert False, 'Unknown type: ' + str(member.type.basetype)
+elif isinstance(member.type.basetype, NamespacedType):
+    type_ = '::'.join(member.type.basetype.namespaces + [member.type.basetype.name])
 }@
-size_t size_function__@(spec.base_type.type)__@(field.name)(const void * untyped_member)
+size_t size_function__@(message.structure.type.name)__@(member.name)(const void * untyped_member)
 {
-@[      if field.type.array_size and not field.type.is_upper_bound]@
+@[    if isinstance(member.type, Array)]@
   (void)untyped_member;
-  return @(field.type.array_size);
-@[      else]@
+  return @(member.type.size);
+@[    else]@
   const auto * member = reinterpret_cast<const std::vector<@(type_)> *>(untyped_member);
   return member->size();
-@[      end if]@
+@[    end if]@
 }
 
-const void * get_const_function__@(spec.base_type.type)__@(field.name)(const void * untyped_member, size_t index)
+const void * get_const_function__@(message.structure.type.name)__@(member.name)(const void * untyped_member, size_t index)
 {
-@[      if field.type.array_size and not field.type.is_upper_bound]@
+@[    if isinstance(member.type, Array)]@
   const auto & member =
-    *reinterpret_cast<const std::array<@(type_), @(field.type.array_size)> *>(untyped_member);
-@[      else]@
+    *reinterpret_cast<const std::array<@(type_), @(member.type.size)> *>(untyped_member);
+@[    else]@
   const auto & member =
     *reinterpret_cast<const std::vector<@(type_)> *>(untyped_member);
-@[      end if]@
+@[    end if]@
   return &member[index];
 }
 
-void * get_function__@(spec.base_type.type)__@(field.name)(void * untyped_member, size_t index)
+void * get_function__@(message.structure.type.name)__@(member.name)(void * untyped_member, size_t index)
 {
-@[      if field.type.array_size and not field.type.is_upper_bound]@
+@[    if isinstance(member.type, Array)]@
   auto & member =
-    *reinterpret_cast<std::array<@(type_), @(field.type.array_size)> *>(untyped_member);
-@[      else]@
+    *reinterpret_cast<std::array<@(type_), @(member.type.size)> *>(untyped_member);
+@[    else]@
   auto & member =
     *reinterpret_cast<std::vector<@(type_)> *>(untyped_member);
-@[      end if]@
+@[    end if]@
   return &member[index];
 }
 
-@[      if not field.type.array_size or field.type.is_upper_bound]@
-void resize_function__@(spec.base_type.type)__@(field.name)(void * untyped_member, size_t size)
+@[    if isinstance(member.type, Sequence)]@
+void resize_function__@(message.structure.type.name)__@(member.name)(void * untyped_member, size_t size)
 {
   auto * member =
     reinterpret_cast<std::vector<@(type_)> *>(untyped_member);
   member->resize(size);
 }
 
-@[      end if]@
 @[    end if]@
-@[  end for]@
-static const ::rosidl_typesupport_introspection_cpp::MessageMember @(spec.base_type.type)_message_member_array[@(len(spec.fields))] = {
+@[  end if]@
+@[end for]@
+static const ::rosidl_typesupport_introspection_cpp::MessageMember @(message.structure.type.name)_message_member_array[@(len(message.structure.members))] = {
 @{
-for index, field in enumerate(spec.fields):
+for index, member in enumerate(message.structure.members):
+    type_ = member.type
+    if isinstance(type_, NestedType):
+        type_ = type_.basetype
+
     print('  {')
 
     # const char * name_
-    print('    "%s",  // name' % field.name)
-    if field.type.is_primitive_type():
+    print('    "%s",  // name' % member.name)
+    if isinstance(type_, BasicType):
         # uint8_t type_id_
-        print('    ::rosidl_typesupport_introspection_cpp::ROS_TYPE_%s,  // type' % field.type.type.upper())
+        print('    ::rosidl_typesupport_introspection_cpp::ROS_TYPE_%s,  // type' % type_.type.upper())
         # size_t string_upper_bound
-        print('    %u,  // upper bound of string' % (field.type.string_upper_bound if field.type.string_upper_bound is not None else 0))
-        # const rosidl_generator_cpp::MessageTypeSupportHandle * members_
-        print('    nullptr,  // members of sub message')
+        print('    0,  // upper bound of string')
+        # const rosidl_generator_c::MessageTypeSupportHandle * members_
+        print('    NULL,  // members of sub message')
+    elif isinstance(type_, BaseString):
+        # uint8_t type_id_
+        if isinstance(type_, String):
+            print('    ::rosidl_typesupport_introspection_cpp::ROS_TYPE_STRING,  // type')
+        elif isinstance(type_, WString):
+            print('    ::rosidl_typesupport_introspection_cpp::ROS_TYPE_WSTRING,  // type')
+        else:
+            assert False, 'Unknown type: ' + str(type_)
+        # size_t string_upper_bound
+        print('    %u,  // upper bound of string' % (type_.maximum_size if type_.maximum_size is not None else 0))
+        # const rosidl_generator_c::MessageTypeSupportHandle * members_
+        print('    NULL,  // members of sub message')
     else:
         # uint8_t type_id_
         print('    ::rosidl_typesupport_introspection_cpp::ROS_TYPE_MESSAGE,  // type')
         # size_t string_upper_bound
         print('    0,  // upper bound of string')
         # const rosidl_message_type_support_t * members_
-        print('    ::rosidl_typesupport_introspection_cpp::get_message_type_support_handle<%s::msg::%s>(),  // members of sub message' % (field.type.pkg_name, field.type.type))
+        print('    ::rosidl_typesupport_introspection_cpp::get_message_type_support_handle<%s>(),  // members of sub message' % '::'.join(type_.namespaces + [type_.name]))
     # bool is_array_
-    print('    %s,  // is array' % ('true' if field.type.is_array else 'false'))
+    print('    %s,  // is array' % ('true' if isinstance(member.type, NestedType) else 'false'))
     # size_t array_size_
-    print('    %u,  // array size' % (field.type.array_size if field.type.array_size else 0))
+    print('    %u,  // array size' % (member.type.size if isinstance(member.type, Array) else (member.type.upper_bound if isinstance(member.type, BoundedSequence) else 0)))
     # bool is_upper_bound_
-    print('    %s,  // is upper bound' % ('true' if field.type.is_upper_bound else 'false'))
+    print('    %s,  // is upper bound' % ('true' if isinstance(member.type, BoundedSequence) else 'false'))
     # unsigned long offset_
-    print('    offsetof(%s::%s::%s, %s),  // bytes offset in struct' % (spec.base_type.pkg_name, subfolder, spec.base_type.type, field.name))
+    print('    offsetof(%s::%s, %s),  // bytes offset in struct' % ('::'.join([package_name] + list(interface_path.parents[0].parts)), message.structure.type.name, member.name))
     # void * default_value_
     print('    nullptr,  // default value')  # TODO default value to be set
 
-    function_suffix = '%s__%s' % (spec.base_type.type, field.name) if field.type.is_array and not is_vector_bool(field) else None
+    function_suffix = ('%s__%s' % (message.structure.type.name, member.name)) if isinstance(member.type, NestedType) and isinstance(member.type.basetype, NamespacedType) else None
 
     # size_t(const void *) size_function
     print('    %s,  // size() function pointer' % ('size_function__%s' % function_suffix if function_suffix else 'nullptr'))
@@ -142,39 +172,34 @@ for index, field in enumerate(spec.fields):
     # void *(void *, size_t) get_function
     print('    %s,  // get(index) function pointer' % ('get_function__%s' % function_suffix if function_suffix else 'nullptr'))
     # void(void *, size_t) resize_function
-    print('    %s  // resize(index) function pointer' % ('resize_function__%s' % function_suffix if function_suffix and (not field.type.array_size or field.type.is_upper_bound) else 'nullptr'))
+    print('    %s  // resize(index) function pointer' % ('resize_function__%s' % function_suffix if function_suffix and isinstance(member.type.basetype, Sequence) else 'NULL'))
 
-    if index < len(spec.fields) - 1:
+    if index < len(message.structure.members) - 1:
         print('  },')
     else:
         print('  }')
 }@
 };
 
-@[end if]@
-static const ::rosidl_typesupport_introspection_cpp::MessageMembers @(spec.base_type.type)_message_members = {
-  "@(spec.base_type.pkg_name)",  // package name
-  "@(spec.base_type.type)",  // message name
-  @(len(spec.fields)),  // number of fields
-  sizeof(@(spec.base_type.pkg_name)::@(subfolder)::@(spec.base_type.type)),
-@[if spec.fields]@
-  @(spec.base_type.type)_message_member_array  // message members
-@[else]@
-  0  // message members
-@[end if]@
+static const ::rosidl_typesupport_introspection_cpp::MessageMembers @(message.structure.type.name)_message_members = {
+  "@(package_name)",  // package name
+  "@(message.structure.type.name)",  // message name
+  @(len(message.structure.members)),  // number of fields
+  sizeof(@('::'.join([package_name] + list(interface_path.parents[0].parts) + [message.structure.type.name]))),
+  @(message.structure.type.name)_message_member_array  // message members
 };
 
-static const rosidl_message_type_support_t @(spec.base_type.type)_message_type_support_handle = {
+static const rosidl_message_type_support_t @(message.structure.type.name)_message_type_support_handle = {
   ::rosidl_typesupport_introspection_cpp::typesupport_identifier,
-  &@(spec.base_type.type)_message_members,
+  &@(message.structure.type.name)_message_members,
   get_message_typesupport_handle_function,
 };
 
 }  // namespace rosidl_typesupport_introspection_cpp
+@[  for ns in reversed(message.structure.type.namespaces)]@
 
-}  // namespace @(subfolder)
-
-}  // namespace @(spec.base_type.pkg_name)
+}  // namespace @(ns)
+@[  end for]@
 
 
 namespace rosidl_typesupport_introspection_cpp
@@ -183,9 +208,9 @@ namespace rosidl_typesupport_introspection_cpp
 template<>
 ROSIDL_TYPESUPPORT_INTROSPECTION_CPP_PUBLIC
 const rosidl_message_type_support_t *
-get_message_type_support_handle<@(spec.base_type.pkg_name)::@(subfolder)::@(spec.base_type.type)>()
+get_message_type_support_handle<@('::'.join([package_name] + list(interface_path.parents[0].parts) + [message.structure.type.name]))>()
 {
-  return &::@(spec.base_type.pkg_name)::@(subfolder)::rosidl_typesupport_introspection_cpp::@(spec.base_type.type)_message_type_support_handle;
+  return &::@('::'.join([package_name] + list(interface_path.parents[0].parts)))::rosidl_typesupport_introspection_cpp::@(message.structure.type.name)_message_type_support_handle;
 }
 
 }  // namespace rosidl_typesupport_introspection_cpp
@@ -197,8 +222,8 @@ extern "C"
 
 ROSIDL_TYPESUPPORT_INTROSPECTION_CPP_PUBLIC
 const rosidl_message_type_support_t *
-ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_cpp, @(spec.base_type.pkg_name), @(subfolder), @(spec.base_type.type))() {
-  return &::@(spec.base_type.pkg_name)::@(subfolder)::rosidl_typesupport_introspection_cpp::@(spec.base_type.type)_message_type_support_handle;
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_cpp, @(', '.join([package_name] + list(interface_path.parents[0].parts) + [message.structure.type.name])))() {
+  return &::@('::'.join([package_name] + list(interface_path.parents[0].parts)))::rosidl_typesupport_introspection_cpp::@(message.structure.type.name)_message_type_support_handle;
 }
 
 #ifdef __cplusplus

commit 0a29a77225711509930ab3fe727ee49e3fc20568

Fix condition for including resize functions
Bug introduced by #334.

Signed-off-by: Jacob Perron <jacob@openrobotics.org>

rosidl_typesupport_introspection_cpp/resource/msg__type_support.cpp.em
@@ -172,7 +172,7 @@ for index, member in enumerate(message.structure.members):
     # void *(void *, size_t) get_function
     print('    %s,  // get(index) function pointer' % ('get_function__%s' % function_suffix if function_suffix else 'nullptr'))
     # void(void *, size_t) resize_function
-    print('    %s  // resize(index) function pointer' % ('resize_function__%s' % function_suffix if function_suffix and isinstance(member.type.value_type, AbstractSequence) else 'NULL'))
+    print('    %s  // resize(index) function pointer' % ('resize_function__%s' % function_suffix if function_suffix and isinstance(member.type, AbstractSequence) else 'NULL'))
 
     if index < len(message.structure.members) - 1:
         print('  },')
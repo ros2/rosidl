f8167cfb9d10f3e055fdc2fe60bad2b50c22f15f
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-06-05 14:20:52 -0700

    fix cpp generator and introspection ts for long double (#383)
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- rosidl_generator_cpp/rosidl_generator_cpp/__init__.py
+++ rosidl_generator_cpp/rosidl_generator_cpp/__init__.py
@@ -181,7 +181,7 @@ def primitive_value_to_cpp(type_, value):
     if type_.typename in [
         'short', 'unsigned short',
         'char', 'wchar',
-        'double',
+        'double', 'long double',
         'octet',
         'int8', 'uint8',
         'int16', 'uint16',

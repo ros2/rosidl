e5253b04ede4dc69d15ce7cc43c815f26d73fc4e
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-07-31 07:39:21 -0700

    use latin-1 encoding when reading/writing .idl files, prepend BOM to generated C/C++ files when necessary (#391)
    * use utf-8 encoding when reading/writing expanded templates
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * inject BOM into generated C/C++ code when necessary
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * use latin-1 encoding for .idl files
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    Signed-off-by: Siddharth Kucheria <kucheria@usc.edu>

--- rosidl_generator_cpp/test/test_srv_initialization.cpp
+++ rosidl_generator_cpp/test/test_srv_initialization.cpp
@@ -1,4 +1,4 @@
-// Copyright 2017 Open Source Robotics Foundation, Inc.
+// Copyright 2019 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,30 +14,48 @@
 
 #include <gtest/gtest.h>
 
-#include "rosidl_generator_cpp/srv/complex_type_zero_fill.hpp"
+#include "rosidl_generator_cpp/srv/arrays.hpp"
+#include "rosidl_generator_cpp/srv/basic_types.hpp"
+#include "rosidl_generator_cpp/srv/empty.hpp"
 
 TEST(Test_srv_initialization, no_arg_request_constructor) {
-  rosidl_generator_cpp::srv::ComplexTypeZeroFill::Request def;
+  rosidl_generator_cpp::srv::Empty::Request empty;
 
-  ASSERT_EQ(2UL, def.req.size());
-  ASSERT_EQ(0UL, def.req[0].x);
-  ASSERT_EQ(0UL, def.req[1].x);
-  ASSERT_EQ(45UL, def.req[0].y);
-  ASSERT_EQ(45UL, def.req[1].y);
-  ASSERT_EQ(0UL, def.req[0].z);
-  ASSERT_EQ(0UL, def.req[1].z);
+  rosidl_generator_cpp::srv::BasicTypes::Request basic_types;
+  EXPECT_EQ(false, basic_types.bool_value);
+  EXPECT_EQ(0, basic_types.byte_value);
+  EXPECT_EQ(0, basic_types.char_value);
+  EXPECT_EQ(0.0f, basic_types.float32_value);
+  EXPECT_EQ(0.0, basic_types.float64_value);
+  EXPECT_EQ(0, basic_types.int8_value);
+  EXPECT_EQ(0u, basic_types.uint8_value);
+  EXPECT_EQ(0, basic_types.int16_value);
+  EXPECT_EQ(0u, basic_types.uint16_value);
+  EXPECT_EQ(0, basic_types.int32_value);
+  EXPECT_EQ(0u, basic_types.uint32_value);
+  EXPECT_EQ(0, basic_types.int64_value);
+  EXPECT_EQ(0u, basic_types.uint64_value);
+  EXPECT_EQ("", basic_types.string_value);
 }
 
 TEST(Test_srv_initialization, no_arg_response_constructor) {
-  rosidl_generator_cpp::srv::ComplexTypeZeroFill::Response def;
+  rosidl_generator_cpp::srv::Empty::Request empty;
 
-  ASSERT_EQ(2UL, def.reply.size());
-  ASSERT_EQ(0UL, def.reply[0].x);
-  ASSERT_EQ(0UL, def.reply[1].x);
-  ASSERT_EQ(45UL, def.reply[0].y);
-  ASSERT_EQ(45UL, def.reply[1].y);
-  ASSERT_EQ(0UL, def.reply[0].z);
-  ASSERT_EQ(0UL, def.reply[1].z);
+  rosidl_generator_cpp::srv::BasicTypes::Response basic_types;
+  EXPECT_EQ(false, basic_types.bool_value);
+  EXPECT_EQ(0, basic_types.byte_value);
+  EXPECT_EQ(0, basic_types.char_value);
+  EXPECT_EQ(0.0f, basic_types.float32_value);
+  EXPECT_EQ(0.0, basic_types.float64_value);
+  EXPECT_EQ(0, basic_types.int8_value);
+  EXPECT_EQ(0u, basic_types.uint8_value);
+  EXPECT_EQ(0, basic_types.int16_value);
+  EXPECT_EQ(0u, basic_types.uint16_value);
+  EXPECT_EQ(0, basic_types.int32_value);
+  EXPECT_EQ(0u, basic_types.uint32_value);
+  EXPECT_EQ(0, basic_types.int64_value);
+  EXPECT_EQ(0u, basic_types.uint64_value);
+  EXPECT_EQ("", basic_types.string_value);
 }
 
 // Note that we very specifically don't add tests for some of the other

2cda518da9d3544c9c5803ea4916f589e4d4c923
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2020-04-10 12:23:12 +0200

    Splitted rosidl_generator_c and rosidl_generator_cpp in two: rosidl_generator_x and rosidl_runtime_x (#442)
    * Splitted rosidl_generator_c and rosidl_generator_cpp in two: rosidl_generator_x and rosidl_runtime_x
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * added comments
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * rosidl_runtime_cpp removed rosidl_parser dependency
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * removed c++ flags
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * included more feedback
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Restoring rosidl_typesupport_interface in the package.xml
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * rosidl_typesupport_interface from build_depend to build_export_depend
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Restored tabs
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * rosidl_generator_c including headers in CMakeLists.txt
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Fixed rosidl_generator_cpp cmakelists
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Exported the right rosidl_generator and rosidl_runtime
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * replaced include_directories for target_include_directories
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * moved rosidl_generator_x dependencies from build_depend to test_depend
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Removed member_of_group rosidl_runtime_packages from rosidl_generator_c
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * changed target_include_libraries and directory for ament_target_dependencies
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * restored add dependency in rosidl_generator_cpp
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Removed rosidl_generator_x from rosidl_typesupport_introspection_x
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Removed comments
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Restored C standard and compiler flags
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * keep the originally used dependency type
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Fixed rosidl_runtime_c cmakelists.txt
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Removed ament_export_dependencies rosidl_cmake
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * fixed test_msg_builder directories
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- rosidl_generator_c/cmake/rosidl_generator_c_generate_interfaces.cmake
+++ rosidl_generator_c/cmake/rosidl_generator_c_generate_interfaces.cmake
@@ -125,7 +125,7 @@ foreach(_pkg_name ${rosidl_generate_interfaces_DEPENDENCY_PACKAGE_NAMES})
     ${_pkg_name})
 endforeach()
 ament_target_dependencies(${rosidl_generate_interfaces_TARGET}${_target_suffix}
-  "rosidl_generator_c"
+  "rosidl_runtime_c"
   "rosidl_typesupport_interface")
 
 add_dependencies(

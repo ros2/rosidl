b4c17898ea47765389d1e6ee1d9415c24b90413d
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-04-22 21:18:37 -0700

    export targets in a addition to include directories / libraries
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- rosidl_typesupport_introspection_cpp/cmake/rosidl_typesupport_introspection_cpp_generate_interfaces.cmake
+++ rosidl_typesupport_introspection_cpp/cmake/rosidl_typesupport_introspection_cpp_generate_interfaces.cmake
@@ -98,6 +98,7 @@ target_include_directories(${rosidl_generate_interfaces_TARGET}${_target_suffix}
   ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_cpp
 )
 ament_target_dependencies(${rosidl_generate_interfaces_TARGET}${_target_suffix}
+  "rosidl_runtime_c"
   "rosidl_typesupport_interface"
   "rosidl_typesupport_introspection_cpp")
 foreach(_pkg_name ${rosidl_generate_interfaces_DEPENDENCY_PACKAGE_NAMES})

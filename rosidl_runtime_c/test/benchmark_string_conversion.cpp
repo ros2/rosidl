002c7e81887ff9fe30b8cd037471320b08a7e597
Scott K Logan <logans@cottsay.net>
2020-09-03 12:06:03 -0700

    Add tests for asymptotic complexity of string conversions
    Signed-off-by: Scott K Logan <logans@cottsay.net>

---
+++ rosidl_runtime_c/test/benchmark_string_conversion.cpp
@@ -0,0 +1,73 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include "rosidl_runtime_c/string_functions.h"
+#include "rosidl_runtime_c/u16string_functions.h"
+
+#include "performance_test_fixture/performance_test_fixture.hpp"
+
+using performance_test_fixture::PerformanceTest;
+
+BENCHMARK_DEFINE_F(PerformanceTest, string_assign_complexity)(benchmark::State & st)
+{
+  size_t len = st.range(0);
+  std::string data(len, '*');
+
+  rosidl_runtime_c__String s;
+  if (!rosidl_runtime_c__String__init(&s)) {
+    st.SkipWithError("String initialization failed");
+    return;
+  }
+
+  // No resize function - just do a copy
+  rosidl_runtime_c__String__assignn(&s, data.c_str(), len);
+
+  for (auto _ : st) {
+    rosidl_runtime_c__String__assignn(&s, data.c_str(), len);
+  }
+
+  st.SetComplexityN(len);
+
+  rosidl_runtime_c__String__fini(&s);
+}
+
+BENCHMARK_REGISTER_F(PerformanceTest, string_assign_complexity)
+->RangeMultiplier(2)->Range(2 << 11, 2 << 19)->Complexity();
+
+BENCHMARK_DEFINE_F(PerformanceTest, u16string_assign_complexity)(benchmark::State & st)
+{
+  size_t len = st.range(0);
+  std::string data(len, '*');
+
+  rosidl_runtime_c__U16String s;
+  if (!rosidl_runtime_c__U16String__init(&s)) {
+    st.SkipWithError("U16String initialization failed");
+    return;
+  }
+
+  rosidl_runtime_c__U16String__resize(&s, len);
+
+  for (auto _ : st) {
+    rosidl_runtime_c__U16String__assignn_from_char(&s, data.c_str(), len);
+  }
+
+  st.SetComplexityN(len);
+
+  rosidl_runtime_c__U16String__fini(&s);
+}
+
+BENCHMARK_REGISTER_F(PerformanceTest, u16string_assign_complexity)
+->RangeMultiplier(2)->Range(2 << 11, 2 << 19)->Complexity();

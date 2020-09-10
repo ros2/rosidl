c71a01c401d9b78cc563c86844dc2692c2953832
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-09-10 09:33:41 -0700

    use static_cast
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- rosidl_runtime_cpp/include/rosidl_runtime_cpp/traits.hpp
+++ rosidl_runtime_cpp/include/rosidl_runtime_cpp/traits.hpp
@@ -130,7 +130,7 @@ void value_to_yaml(const std::u16string & value, std::ostream & out)
   auto flags = out.flags();
   size_t index = 0;
   while (index < value.size()) {
-    int16_t character = *reinterpret_cast<const int16_t *>(&value[index]);
+    int16_t character = static_cast<int16_t>(value[index]);
     if (!(character & 0xff80)) {  // ASCII
       std::string character_as_string = convert.to_bytes(character);
       out << std::hex << character_as_string.c_str();

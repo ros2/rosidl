1c71684fa7324bc5d2089f6216e49fc7fbda610e
Michel Hidalgo <michel@ekumenlabs.com>
2021-03-09 16:03:45 -0300

    Align rosidl_cli package version with the rest of the repo. (#579)
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- rosidl_cli/setup.py
+++ rosidl_cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rosidl_cli',
-    version='0.1.0',
+    version='2.0.3',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],

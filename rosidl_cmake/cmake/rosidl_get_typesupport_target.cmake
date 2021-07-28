997490da12d33059b60cbc63a528b269c9de3f39
Shane Loretz <sloretz@openrobotics.org>
2021-07-28 08:23:20 -0700

    Shorten line length
    Signed-off-by: Shane Loretz <sloretz@openrobotics.org>

--- rosidl_cmake/cmake/rosidl_get_typesupport_target.cmake
+++ rosidl_cmake/cmake/rosidl_get_typesupport_target.cmake
@@ -27,7 +27,7 @@
 function(rosidl_get_typesupport_target var generate_interfaces_target typesupport_name)
   if(NOT TARGET ${generate_interfaces_target})
     message(FATAL_ERROR
-      "${generate_interfaces_target} is not a CMake target - maybe rosidl_generate_interfaces wasn't called or was given a different target name?")
+      "${generate_interfaces_target} is not a CMake target. Maybe rosidl_generate_interfaces was given a different target name?")
   endif()
 
   set(output_target "${generate_interfaces_target}__${typesupport_name}")

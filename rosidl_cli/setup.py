62fc0bd06854e1b3f3cf64d0af33f57ad75632fe
Chris Lalancette <clalancette@openrobotics.org>
2022-04-05 17:43:46 +0000

    3.1.2

--- rosidl_cli/setup.py
+++ rosidl_cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rosidl_cli',
-    version='3.1.1',
+    version='3.1.2',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],

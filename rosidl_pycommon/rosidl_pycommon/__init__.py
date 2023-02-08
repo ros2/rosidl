9d4636a5a10499848db2fd96d4645d932b8a4339
Emerson Knapp <emerson.b.knapp@gmail.com>
2023-02-07 16:39:46 -0800

    Code cleanup pass
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rosidl_pycommon/rosidl_pycommon/__init__.py
+++ rosidl_pycommon/rosidl_pycommon/__init__.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_index_python import get_package_share_directory
-
 from io import StringIO
 import json
 import os

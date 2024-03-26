1ac07aad4fb38a1734755921c1d815c79f4b2a1a
Mario Dominguez <mariodominguez@eprosima.com>
2024-03-26 15:21:08 +0000

    Add has_any_member_with_annotation() method
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- rosidl_parser/rosidl_parser/definition.py
+++ rosidl_parser/rosidl_parser/definition.py
@@ -508,6 +508,15 @@ class Structure(Annotatable):
         self.namespaced_type = namespaced_type
         self.members = members or []
 
+    def has_any_member_with_annotation(self, name: str):
+        """
+        Returns whether any member has a particular annotation.
+
+        :param name: the annotation name
+        """
+        has_any = [member.name for member in self.members if member.has_annotation(name)]
+        return bool(has_any)
+
 
 class Include:
     """An include statement."""

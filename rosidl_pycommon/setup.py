a0c57e6847a7ed6e70203e2b271a8bcc1db54d59
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-29 00:09:31 +0200

    fix setuptools deprecation (#880)
    Signed-off-by: mosfet80 <10235105+mosfet80@users.noreply.github.com>
    (cherry picked from commit 13a7ca4e8b9a81beaa2bc6b79d7399537e7598e4)
    
    # Conflicts:
    #	rosidl_pycommon/setup.py

---
+++ rosidl_pycommon/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'rosidl_pycommon'
+
+setup(
+    name=package_name,
+    version='5.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Jacob Perron',
+    author_email='jacob@openrobotics.org',
+    maintainer='Aditya Pande, Brandon Ong, Dharini Dutia, Shane Loretz',
+    maintainer_email='aditya.pande@openrobotics.org, brandon@openrobotics.org, dharini@openrobotics.org, sloretz@openrobotics.org',  # noqa: E501
+    description='Common Python functions used by rosidl packages.',
+    license='Apache License, Version 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)

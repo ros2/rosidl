42abc98cba7af7ad176670de1555fa54d657e12f
Chris Lalancette <clalancette@gmail.com>
2026-05-06 18:14:02 +0000

    5.3.0
    Signed-off-by: Vincenzo Petrone <vipetrone@unisa.it>

--- rosidl_pycommon/setup.py
+++ rosidl_pycommon/setup.py
@@ -5,7 +5,7 @@ package_name = 'rosidl_pycommon'
 
 setup(
     name=package_name,
-    version='5.2.0',
+    version='5.3.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',

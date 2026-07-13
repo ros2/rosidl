76644923a5ce05e21c78dc629c7952e02f790731
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-07-13 16:31:07 +0200

    4.9.8

--- rosidl_cli/setup.py
+++ rosidl_cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rosidl_cli',
-    version='4.9.7',
+    version='4.9.8',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
